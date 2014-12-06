0ed9749fe66edd07d9122c4a8b59f9215c4393af
Federico Ravasio <ravasio.federico@gmail.com>
2014-12-06 20:15:36 +0100

    Added if evaluation with arithy 2.

--- tests/eval_special_forms.rs
+++ tests/eval_special_forms.rs
@@ -31,7 +31,9 @@ fn if_() {
     assert_eval("(if #f a (+ 1))", integer(1));
     assert_eval("(if (and #t #f) a 1)", integer(1));
 
+    assert_eval("(if #t 3)", integer(3));
+    assert_eval("(if #f 3)", bool(false)); // unspecified behaviour
+
     assert_eval_err("(if)", bad_arity("if"));
-    assert_eval_err("(if a b)", bad_arity("if"));
     assert_eval_err("(if a b c d)", bad_arity("if"));
 }
